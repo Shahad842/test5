0065051934252901c6b15648c15e5c0731a3d642
Shahad <shahad_9215@hotmail.com>
2023-10-05 12:17:06 +0300

    Initial Commit

---
+++ test5/test5App.swift
@@ -0,0 +1,17 @@
+//
+//  test5App.swift
+//  test5
+//
+//  Created by Shahad Alhothali on 20/03/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct test5App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
